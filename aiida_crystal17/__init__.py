50dfc89df98b31604c57448d23d028c7a8bb0b17
Chris Sewell <chrisj_sewell@hotmail.com>
2018-08-23 16:25:04 +0100

    updated setup information

--- aiida_crystal17/__init__.py
+++ aiida_crystal17/__init__.py
@@ -4,4 +4,4 @@ AiiDA Plugin Template
 Adapt this template for your own needs.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from importlib import import_module
 import json
 
 if __name__ == '__main__':
@@ -8,6 +9,7 @@ if __name__ == '__main__':
     # such that it can be discovered automatically
     with open('setup.json', 'r') as info:
         kwargs = json.load(info)
+    kwargs["version"] = import_module('aiida_crystal17').__version__
     setup(
         packages=find_packages(),
         **kwargs
