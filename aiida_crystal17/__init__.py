da50c2be28b033d70e8bd73dd7b9ae73803bf310
Chris Sewell <chrisj_sewell@hotmail.com>
2018-09-06 00:24:57 +0100

    removed diff modules and updated version

--- aiida_crystal17/__init__.py
+++ aiida_crystal17/__init__.py
@@ -4,21 +4,4 @@ aiida_crystal17
 AiiDA plugin for running the CRYSTAL17 code
 """
 
-__version__ = "0.2.0a0"
-
-# TODO delete all the template files (and remove from entry_points and scripts):
-# aiida_crystal17/
-#     data/
-#         data_cli.py
-#         diff_params.py
-#     calculations/
-#         diffcalc.py
-#     parsers/
-#         diffparser.py
-#     tests/
-#         file1.txt
-#         file2.txt
-#         test_calculations.py
-# examples/
-#     cli.py
-#     submit.py
+__version__ = "0.3.0a0"
