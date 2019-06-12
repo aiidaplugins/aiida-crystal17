f1621cc7ad2838761a52e6c584cadb93d95cdbae
Chris Sewell <chrisj_sewell@hotmail.com>
2019-06-12 10:34:53 +0100

    update ICL scheduler

--- aiida_crystal17/schedulers/pbspro_icl.py
+++ aiida_crystal17/schedulers/pbspro_icl.py
@@ -17,6 +17,7 @@ from __future__ import division
 from __future__ import absolute_import
 import logging
 from aiida.schedulers.plugins.pbsbaseclasses import PbsBaseClass
+from aiida.schedulers.datastructures import JobResource
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -37,23 +38,87 @@ _LOGGER = logging.getLogger(__name__)
 # X  Subjob has completed execution or has been deleted.
 
 
+class ICLJobResource(JobResource):
+    """
+    Base class for PBS job resources on the ICL (cx) HPCs
+    """
+    _default_fields = (
+        'num_machines',
+        'num_cores_per_machine'
+    )
+
+    @classmethod
+    def get_valid_keys(cls):
+        """
+        Return a list of valid keys to be passed to the __init__
+        """
+        return super(ICLJobResource, cls).get_valid_keys() + []
+
+    @classmethod
+    def accepts_default_mpiprocs_per_machine(cls):
+        """
+        Return True if this JobResource accepts a 'default_mpiprocs_per_machine'
+        key, False otherwise.
+        """
+        return False
+
+    def __init__(self, **kwargs):
+        """
+        Initialize the job resources from the passed arguments (the valid keys can be
+        obtained with the function self.get_valid_keys()).
+
+        Should raise only ValueError or TypeError on invalid parameters.
+        """
+        super(ICLJobResource, self).__init__()
+
+        if 'num_machines' not in kwargs:
+            raise TypeError('num_machines must be specified')
+        if 'num_cores_per_machine' not in kwargs:
+            raise TypeError('num_cores_per_machine must be specified')
+
+        try:
+            self.num_machines = int(kwargs.pop('num_machines'))
+        except ValueError:
+            raise ValueError("num_machines must an integer")
+
+        try:
+            self.num_cores_per_machine = int(kwargs.pop('num_cores_per_machine'))
+        except ValueError:
+            raise ValueError("num_cores_per_machine must an integer")
+
+        if kwargs:
+            raise TypeError("The following parameters were not recognized for "
+                            "the JobResource: {}".format(kwargs.keys()))
+
+        if self.num_machines <= 0:
+            raise ValueError("num_machine must be >= 1")
+        if self.num_cores_per_machine <= 0:
+            raise ValueError("num_cores_per_machine must be >= 1")
+
+        self.num_mpiprocs_per_machine = None
+
+
 class PbsproICLScheduler(PbsBaseClass):
     """
     Subclass to support the PBSPro scheduler
     (http://www.pbsworks.com/).
-    But altered to fit the Imperial College London cx scheduler spec
+    But altered to fit the Imperial College London cx scheduler spec,
+    which requires ncpus and mem to be defines
+    See:
+    https://www.imperial.ac.uk/admin-services/ict/self-service/research-support/rcs/computing/high-throughput-computing/job-sizing/
 
     I redefine only what needs to change from the base class
     """
 
     # I don't need to change this from the base class
-    # _job_resource_class = PbsJobResource
+    _job_resource_class = ICLJobResource
 
     # For the time being I use a common dictionary, should be sufficient
     # for the time being, but I can redefine it if needed.
     # _map_status = _map_status_pbs_common
 
-    def _get_resource_lines(self, num_machines, num_mpiprocs_per_machine, num_cores_per_machine, max_memory_kb,
+    def _get_resource_lines(self, num_machines, num_mpiprocs_per_machine,
+                            num_cores_per_machine, max_memory_kb,
                             max_wallclock_seconds):
         """
         Return the lines for machines, memory and wallclock relative
@@ -65,12 +130,6 @@ class PbsproICLScheduler(PbsBaseClass):
         return_lines = []
 
         select_string = "select={}".format(num_machines)
-
-        # This is used in the aiida-core PbsproScheduler, but ICL CX only accepts ncpus
-        # if num_mpiprocs_per_machine:
-        #     select_string += ":mpiprocs={}".format(num_mpiprocs_per_machine)
-        # if num_cores_per_machine:
-        #     select_string += ":ppn={}".format(num_cores_per_machine)
 
         if num_cores_per_machine is not None and num_cores_per_machine > 0:
             select_string += ":ncpus={}".format(num_cores_per_machine)
@@ -91,18 +150,21 @@ class PbsproICLScheduler(PbsBaseClass):
             tot_minutes = tot_secs % 3600
             minutes = tot_minutes // 60
             seconds = tot_minutes % 60
-            return_lines.append("#PBS -l walltime={:02d}:{:02d}:{:02d}".format(hours, minutes, seconds))
+            return_lines.append(
+                "#PBS -l walltime={:02d}:{:02d}:{:02d}".format(hours, minutes, seconds))
 
-        if max_memory_kb:
-            try:
-                virtual_memory_kb = int(max_memory_kb)
-                if virtual_memory_kb <= 0:
-                    raise ValueError
-            except ValueError:
-                raise ValueError("max_memory_kb must be "
-                                 "a positive integer (in kB)! It is instead '{}'"
-                                 "".format((max_memory_kb)))
-            select_string += ":mem={}kb".format(virtual_memory_kb)
+        if not max_memory_kb:
+            max_memory_kb = 1000  # use a default memory of 1gb
+
+        try:
+            virtual_memory_gb = int(max_memory_kb / 1000.)
+            if virtual_memory_gb <= 0:
+                raise ValueError
+        except ValueError:
+            raise ValueError("max_memory_kb must be "
+                             "a positive integer (in kB) >= 1000 kb! It is instead '{}'"
+                             "".format((max_memory_kb)))
+        select_string += ":mem={}gb".format(virtual_memory_gb)
 
         return_lines.append("#PBS -l {}".format(select_string))
         return return_lines
--- aiida_crystal17/schedulers/test_pbspro_icl.py
+++ aiida_crystal17/schedulers/test_pbspro_icl.py
@@ -9,10 +9,13 @@ def test_submit_script():
     scheduler = PbsproICLScheduler()
 
     job_tmpl = JobTemplate()
-    job_tmpl.shebang = '#!/bin/bash -l'
-    job_tmpl.job_resource = scheduler.create_job_resource(num_machines=1, num_cores_per_machine=1, default_mpiprocs_per_machine=16)
     job_tmpl.uuid = str(uuid.uuid4())
-    job_tmpl.max_wallclock_seconds = 24 * 3600
+    job_tmpl.shebang = '#!/bin/bash -l'
+    job_tmpl.job_resource = scheduler.create_job_resource(
+        num_machines=1, num_cores_per_machine=32)
+    job_tmpl.max_memory_kb = 2000
+    job_tmpl.max_wallclock_seconds = 24.5 * 3600
+    job_tmpl.queue_name = "myqueue"
     code_info = CodeInfo()
     code_info.cmdline_params = ["mpirun", "-np", "23", "pw.x", "-npool", "1"]
     code_info.stdin_name = 'aiida.in'
@@ -21,5 +24,6 @@ def test_submit_script():
 
     submit_script_text = scheduler.get_submit_script(job_tmpl).splitlines()
 
-    assert "#PBS -l select=1:ncpus=1" in submit_script_text
-    assert '#PBS -l walltime=24:00:00' in submit_script_text
+    assert "#PBS -l select=1:ncpus=32:mem=2gb" in submit_script_text
+    assert '#PBS -l walltime=24:30:00' in submit_script_text
+    assert '#PBS -q myqueue' in submit_script_text
