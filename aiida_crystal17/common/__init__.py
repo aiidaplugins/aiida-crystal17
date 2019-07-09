591daa0913b29fbe3a69c5da90c08daa292b14d6
Chris Sewell <chrisj_sewell@hotmail.com>
2019-07-09 12:33:01 +0100

    remove aiida utils
    They are now part of aiida-core

--- aiida_crystal17/common/__init__.py
+++ aiida_crystal17/common/__init__.py
@@ -1,5 +1,4 @@
 """ common functionality """
-from .aiida import get_calc_log  # noqa: F401
 from .atoms import ATOMIC_NUM2SYMBOL  # noqa: F401
 from .dict_funcs import (  # noqa: F401
     get_keys, flatten_dict, unflatten_dict, display_json)
