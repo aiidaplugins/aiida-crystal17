7e462cecae916e5e2c5ec88e9240b17de86b14ef
Chris Sewell <chrisj_sewell@hotmail.com>
2019-07-11 14:23:21 +0100

    add checks for index keys

--- aiida_crystal17/gulp/potentials/base.py
+++ aiida_crystal17/gulp/potentials/base.py
@@ -59,4 +59,23 @@ class PotentialWriterAbstract(object):
         """
         schema = self.get_schema()
         validate_against_schema(data, schema)
+        # test that e.g. '1-2' and '2-1' aren't present
+        if "2body" in data:
+            bonds = []
+            for indices in data["2body"]:
+                index_set = set(indices.split("-"))
+                if index_set in bonds:
+                    raise ValueError(
+                        "both {0}-{1} and {1}-{0} 2body keys exist in the data".format(*index_set))
+                bonds.append(index_set)
+        # test that e.g. '1-2-3' and '3-2-1' aren't present (2 is the pivot atom)
+        if "3body" in data:
+            angles = []
+            for indices in data["3body"]:
+                i1, i2, i3 = indices.split("-")
+                if (i1, i2, i3) in angles:
+                    raise ValueError(
+                        "both {0}-{1}-{2} and {2}-{1}-{0} 3body keys exist in the data".format(i1, i2, i3))
+                angles.append((i1, i2, i3))
+                angles.append((i3, i2, i1))
         return self._make_string(data, species_filter)
