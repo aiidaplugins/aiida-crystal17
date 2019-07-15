c31641a59fcd906d837582b2c2a6304f112ab510
Chris Sewell <chrisj_sewell@hotmail.com>
2019-07-16 00:14:08 +0100

    add reading of lennard potential files

--- aiida_crystal17/gulp/potentials/base.py
+++ aiida_crystal17/gulp/potentials/base.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import copy
+import re
 
 from aiida_crystal17.validation import validate_against_schema
 from aiida_crystal17.gulp.potentials.common import filter_by_species
@@ -17,6 +18,9 @@ number_flagged: int
     number of variables flagged to fit
 
 """
+
+RE_SYMBOL = "([A-Z][a-z]?)"
+RE_SYMBOL_TYPE = "([A-Z][a-z]?)\\s+(\\bc\\b|\\bcore\\b|\\bs\\b|\\bshell\\b)"
 
 
 class PotentialWriterAbstract(object):
@@ -162,3 +166,101 @@ class PotentialWriterAbstract(object):
             # TODO same checks as main data and possibly switch 2body/3body indices to line up with those for main data
 
         return self._make_string(data, fitting_data=fitting_data)
+
+    def read_exising(self, lines):
+        """read an existing potential file
+
+        Parameters
+        ----------
+        lines : list[str]
+
+        Returns
+        -------
+        dict
+            the potential data
+
+        Raises
+        ------
+        IOError
+            on parsing failure
+
+        """
+        raise NotImplementedError
+
+    def _read_section(self, lines, lineno, breaking_terms, number_atoms, global_args=None):
+        """read a section of a potential file, e.g.
+
+        ::
+
+            H core  He shell 1.00000000E+00 2.00000000E+00 12.00000 0 1
+            H B 3.00000000E+00 4.00000000E+00 0.00 12.00000 1 0
+
+        Parameters
+        ----------
+        lines : list[str]
+            the lines in the file
+        lineno : int
+            the current line number, should be the line below the option line
+        breaking_terms : list[str]
+            stop reading lines, if a line starts with one of these terms
+        number_atoms : the number of atoms expected
+            [description]
+        global_args : dict
+            additional arguments to add to the result of each line
+
+        Returns
+        -------
+        int: lineno
+            the final line of the section
+        set: species_set
+            a set of species identified in the section
+        dict: results
+            {tuple[species]: {"values": str, "global": global_args}}
+
+        Raises
+        ------
+        IOError
+            If a parsing error occurs
+
+        """
+        results = {}
+        symbol_set = set()
+
+        while lineno < len(lines):
+            line = lines[lineno]
+            if any([line.strip().startswith(term) for term in breaking_terms]):
+                break
+            # TODO ignore comments at end of line
+            match_sym_type = re.findall(
+                "^{}\\s+(.+)\\s*$".format(
+                    "\\s+".join([RE_SYMBOL_TYPE for _ in range(number_atoms)])),
+                line.strip())
+            match_sym = re.findall(
+                "^{}\\s+(.+)\\s*$".format(
+                    "\\s+".join([RE_SYMBOL for _ in range(number_atoms)])),
+                line.strip())
+            # TODO also match atomic numbers
+            if match_sym_type:
+                result = list(match_sym_type[0])
+                index = []
+                for _ in range(number_atoms):
+                    symbol = result[0]
+                    stype = {"c": "core", "s": "shell"}[result[1][0]]
+                    index.append("{} {}".format(symbol, stype))
+                    result = result[2:]
+                results[tuple(index)] = {"values": result[0], "global": global_args}
+            elif match_sym:
+                result = list(match_sym[0])
+                index = []
+                for _ in range(number_atoms):
+                    symbol = result[0]
+                    index.append("{} {}".format(symbol, "core"))
+                    result = result[1:]
+                results[tuple(index)] = {"values": result[0], "global": global_args}
+            else:
+                raise IOError(
+                    "expected line to be of form "
+                    "'symbol1 <type> symbol2 <type> ... variables': {}".format(line))
+            symbol_set.update(index)
+            lineno += 1
+        return lineno - 1, symbol_set, results
