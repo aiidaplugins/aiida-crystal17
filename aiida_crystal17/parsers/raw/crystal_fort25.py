a8b39ab88590c87974df5492ce2c49a53a70f43d
Chris Sewell <chrisj_sewell@hotmail.com>
2019-07-18 12:31:29 +0100

    upgraded to aiida-core==1.0.0b5

--- aiida_crystal17/parsers/raw/crystal_fort25.py
+++ aiida_crystal17/parsers/raw/crystal_fort25.py
@@ -20,24 +20,26 @@ def parse_crystal_fort25(content):
     -----
     File Format:
 
-    1ST RECORD : -%-,IHFERM,TYPE,NROW,NCOL,DX,DY,COSXY (format : A3,I1,A4,2I5,1P,(3E12.5))
-    2ND RECORD : X0,Y0 (format : 1P,6E12.5)
-    3RD RECORD : I1,I2,I3,I4,I5,I6 (format : 6I3)
-    4TH RECORD
-    AND FOLLOWING : ((RDAT(I,J),I=1,NROW),J=1,NCOL) (format : 1P,6E12.5)
+    ::
 
-    Meaning of the variables:
-    1   NROW            1 (DOSS are written one projection at a time)
-        NCOL            number of energy points in which the DOS is calculated
-        DX              energy increment (hartree)
-        DY              not used
-        COSXY           Fermi energy (hartree)
-    2   X0              energy corresponding to the first point
-        Y0              not used
-    3   I1              number of the projection;
-        I2              number of atomic orbitals of the projection;
-        I3,I4,I5,I6     not used
-    4   RO(J),J=1,NCOL  DOS: density of states ro(eps(j)) (atomic units).
+        1ST RECORD : -%-,IHFERM,TYPE,NROW,NCOL,DX,DY,COSXY (format : A3,I1,A4,2I5,1P,(3E12.5))
+        2ND RECORD : X0,Y0 (format : 1P,6E12.5)
+        3RD RECORD : I1,I2,I3,I4,I5,I6 (format : 6I3)
+        4TH RECORD
+        AND FOLLOWING : ((RDAT(I,J),I=1,NROW),J=1,NCOL) (format : 1P,6E12.5)
+
+        Meaning of the variables:
+        1   NROW            1 (DOSS are written one projection at a time)
+            NCOL            number of energy points in which the DOS is calculated
+            DX              energy increment (hartree)
+            DY              not used
+            COSXY           Fermi energy (hartree)
+        2   X0              energy corresponding to the first point
+            Y0              not used
+        3   I1              number of the projection;
+            I2              number of atomic orbitals of the projection;
+            I3,I4,I5,I6     not used
+        4   RO(J),J=1,NCOL  DOS: density of states ro(eps(j)) (atomic units).
 
     """
     system_type = None
@@ -61,13 +63,11 @@ def parse_crystal_fort25(content):
             if system_type is None:
                 system_type = line[3]
             elif not system_type == line[3]:
-                raise IOError(
-                    "projection {0} has different system type ({1}) to previous ({2})"
-                    .format(proj_number, line[3], system_type))
+                raise IOError("projection {0} has different system type ({1}) to previous ({2})".format(
+                    proj_number, line[3], system_type))
 
             if not line[4:8] == "DOSS":
-                raise IOError(
-                    "projection {0} is not of type DOSS".format(proj_number))
+                raise IOError("projection {0} is not of type DOSS".format(proj_number))
 
             nrows, ncols, _, denergy, fermi = split_numbers(line[8:])
             # nrows, ncols = (int(nrows), int(ncols))
@@ -75,15 +75,13 @@ def parse_crystal_fort25(content):
             if energy_delta is None:
                 energy_delta = denergy
             elif not energy_delta == denergy:
-                raise IOError(
-                    "projection {0} has different delta energy ({1}) to previous ({2})"
-                    .format(proj_number, denergy, energy_delta))
+                raise IOError("projection {0} has different delta energy ({1}) to previous ({2})".format(
+                    proj_number, denergy, energy_delta))
             if fermi_energy is None:
                 fermi_energy = fermi
             elif not fermi_energy == fermi:
-                raise IOError(
-                    "projection {0} has different fermi energy ({1}) to previous ({2})"
-                    .format(proj_number, fermi, fermi_energy))
+                raise IOError("projection {0} has different fermi energy ({1}) to previous ({2})".format(
+                    proj_number, fermi, fermi_energy))
 
             lineno += 1
             line = lines[lineno].strip()
@@ -93,9 +91,8 @@ def parse_crystal_fort25(content):
             if initial_energy is None:
                 initial_energy = ienergy
             elif not initial_energy == ienergy:
-                raise IOError(
-                    "projection {0} has different initial energy ({1}) to previous ({2})"
-                    .format(proj_number, ienergy, initial_energy))
+                raise IOError("projection {0} has different initial energy ({1}) to previous ({2})".format(
+                    proj_number, ienergy, initial_energy))
 
             lineno += 1
             line = lines[lineno].strip()
@@ -116,26 +113,15 @@ def parse_crystal_fort25(content):
             if len_dos is None:
                 len_dos = len(dos)
             elif not len_dos == len(dos):
-                raise IOError(
-                    "projection {0} has different dos value lengths ({1}) to previous ({2})"
-                    .format(proj_number, len(dos), len_dos))
+                raise IOError("projection {0} has different dos value lengths ({1}) to previous ({2})".format(
+                    proj_number, len(dos), len_dos))
 
             if projid not in alpha_projections:
-                alpha_projections[projid] = {
-                    "id": projid,
-                    "norbitals": norbitals,
-                    "dos": dos
-                }
+                alpha_projections[projid] = {"id": projid, "norbitals": norbitals, "dos": dos}
             elif projid in beta_projections:
-                raise IOError(
-                    "three data sets with same projid ({0}) were found".format(
-                        projid))
+                raise IOError("three data sets with same projid ({0}) were found".format(projid))
             else:
-                beta_projections[projid] = {
-                    "id": projid,
-                    "norbitals": norbitals,
-                    "dos": dos
-                }
+                beta_projections[projid] = {"id": projid, "norbitals": norbitals, "dos": dos}
         else:
             lineno += 1
 
@@ -145,9 +131,7 @@ def parse_crystal_fort25(content):
     energy_delta = convert_units(float(energy_delta), "hartree", "eV")
     initial_energy = convert_units(float(initial_energy), "hartree", "eV")
     len_dos = int(len_dos)
-    energies = np.linspace(initial_energy,
-                           initial_energy + len_dos * energy_delta,
-                           len_dos).tolist()
+    energies = np.linspace(initial_energy, initial_energy + len_dos * energy_delta, len_dos).tolist()
 
     total_alpha = None
     total_beta = None
@@ -161,20 +145,13 @@ def parse_crystal_fort25(content):
             "conversion": "CODATA2014",
             "energy": "eV"
         },
-        "energy":
-        energies,
-        "system_type":
-        system_type,
-        "fermi_energy":
-        fermi_energy,
-        "total_alpha":
-        total_alpha,
-        "total_beta":
-        total_beta,
-        "projections_alpha":
-        list(alpha_projections.values()) if alpha_projections else None,
-        "projections_beta":
-        list(beta_projections.values()) if beta_projections else None,
+        "energy": energies,
+        "system_type": system_type,
+        "fermi_energy": fermi_energy,
+        "total_alpha": total_alpha,
+        "total_beta": total_beta,
+        "projections_alpha": list(alpha_projections.values()) if alpha_projections else None,
+        "projections_beta": list(beta_projections.values()) if beta_projections else None,
     }
 
 
@@ -193,8 +170,7 @@ def parse_crystal_fort25_aiida(fileobj, parser_class):
         read_data = parse_crystal_fort25(fileobj.read())
     except IOError as err:
         traceback.print_exc()
-        results_data["parser_errors"].append(
-            "Error parsing CRYSTAL 17 main output: {0}".format(err))
+        results_data["parser_errors"].append("Error parsing CRYSTAL 17 main output: {0}".format(err))
         return results_data, None
 
     results_data["fermi_energy"] = read_data["fermi_energy"]
@@ -219,9 +195,7 @@ def parse_crystal_fort25_aiida(fileobj, parser_class):
         results_data["spin"] = False
 
     if read_data["projections_alpha"] is not None:
-        results_data["norbitals_projections"] = [
-            p["norbitals"] for p in read_data["projections_alpha"]
-        ]
+        results_data["norbitals_projections"] = [p["norbitals"] for p in read_data["projections_alpha"]]
         projected_alpha = [p["dos"] for p in read_data["projections_alpha"]]
     if read_data["projections_beta"] is not None:
         projected_beta = [p["dos"] for p in read_data["projections_beta"]]
