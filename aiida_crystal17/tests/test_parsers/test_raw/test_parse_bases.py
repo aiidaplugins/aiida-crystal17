b1d9dec7e80c8ae5b900950209483085710eeb29
Chris Sewell <chrisj_sewell@hotmail.com>
2019-08-01 05:10:16 +0100

    add ``compute_orbitals`` function

--- aiida_crystal17/tests/test_parsers/test_raw/test_parse_bases.py
+++ aiida_crystal17/tests/test_parsers/test_raw/test_parse_bases.py
@@ -27,3 +27,16 @@ def test_full_files(name, filepath, data_regression):
     output = parse_bases.parse_bsets_stdin(content, isolated=False)
     output = recursive_round(output, 12)
     data_regression.check(output)
+
+
+@pytest.mark.parametrize('name,filepath,atoms', (
+    ('mgo_sto3g', ('crystal', 'mgo_sto3g_scf', 'INPUT'), (12, 8)),
+    ('nio_sto3g', ('crystal', 'nio_sto3g_afm_scf', 'INPUT'), (28, 28, 8, 8)),
+))
+def test_compute_orbitals(name, filepath, atoms, data_regression):
+    path = os.path.join(TEST_FILES, *filepath)
+    with open(path) as handle:
+        content = handle.read()
+    basis_sets = parse_bases.parse_bsets_stdin(content, isolated=False)
+    orbitals = parse_bases.compute_orbitals(atoms, basis_sets)
+    data_regression.check(dict(orbitals._asdict()))
